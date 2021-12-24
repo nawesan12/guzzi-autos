dcebdc825d13b2ff039483679e64670869d053ef
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2021-12-24 16:20:55 -0300

    app creation

---
+++ components/AppLayout/AppLayout.jsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Footer from './Footer'
+
+export default function AppLayout({ children }) {
+    return(
+        <>
+        <Header/>
+        <main>
+            {children}
+        </main>
+        <Footer/>
+        </>
+    )
+}
---
+++ components/AppLayout/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p>Guzzi Autos - 2022</p>
+        </footer>
+    )
+}
