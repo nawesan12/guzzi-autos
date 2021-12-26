113706d4156269dfacd3a2fbada7e1fbbe54d088
nawesan12 <nahueltecnicomdp@gmail.com>
2021-12-26 19:48:44 -0300

    contact working

--- components/AppLayout/Footer.jsx
+++ components/AppLayout/Footer.jsx
@@ -4,35 +4,45 @@ export default function Footer(){
     return(
         <>
         <footer className="app_footer">
-            <p>Ricardo Guzzi - 2022</p>
-            <div className="social_media">
-                <Link href="https://www.google.com"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-whatsapp" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
-                <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
-                </svg></a></Link>
-                <Link href="https://www.instagram.com/guzziautos"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <rect x="4" y="4" width="16" height="16" rx="4" />
-                <circle cx="12" cy="12" r="3" />
-                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
-                </svg></a></Link>
-                <Link href="https://www.facebook.com/Guzzi-Autos-112314927616800/?ref=page_internal"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                </svg></a></Link>
+            <div className="fix-row">
+                <p>Ricardo Guzzi - 2022</p>
+                <div className="social_media">
+                    <Link href="https://www.google.com"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-whatsapp" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+                    <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+                    </svg></a></Link>
+                    <Link href="https://www.instagram.com/guzziautos"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <rect x="4" y="4" width="16" height="16" rx="4" />
+                    <circle cx="12" cy="12" r="3" />
+                    <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                    </svg></a></Link>
+                    <Link href="https://www.facebook.com/Guzzi-Autos-112314927616800/?ref=page_internal"><a><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                    </svg></a></Link>
+                </div>
             </div>
             <span>Pagina hecha por Nahuel Santillan</span>
         </footer>
         <style jsx>{`
             .app_footer {
                 display:flex;
+                flex-direction:column;
+                justify-content:center;
                 align-items:center;
                 justify-content:space-evenly;
                 background:#101010;
                 color:#f5f5f5;    
                 height:10vh;
                 width:100vw;
+            }
+
+            .fix-row {
+                width:100vw;
+                display:flex;
+                justify-content:space-around;
             }
 
             .app_footer p {
@@ -46,8 +56,6 @@ export default function Footer(){
 
             span {
                 font-size:.6rem;
-                bottom:.4rem;
-                position:absolute;
             }
         `}</style>
         </>
--- components/Contacto/Main.jsx
+++ components/Contacto/Main.jsx
@@ -5,37 +5,37 @@ export default function Main() {
         <>
             <section className="contacto_main">
                 <section className="contact_ways">
-                    <article className="contact">
+                    <a className="contact" href="https://api.whatsapp.com/send?phone=+542235254339&text=Hola%20Ricardo!%20Queria%20hacerte%20una%20consulta.">
                         <svg xmlns="http://www.w3.org/2000/svg" className="whatsapp icon icon-tabler icon-tabler-brand-whatsapp" width="44" height="44" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
                             <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
                         </svg>
-                        <a className="link whatsapp">Enviar Mensaje</a>
-                    </article>
-                    <article className="contact">
+                        <span className="link whatsapp">Enviar Mensaje</span>
+                    </a>
+                    <a className="contact" href="callto:+542235254339">
                         <svg xmlns="http://www.w3.org/2000/svg" className="llamar icon icon-tabler icon-tabler-phone" width="44" height="44" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
                         </svg>
-                        <a className="link llamar">Llamar</a>
-                    </article>
-                    <article className="contact">
+                        <span className="link llamar">Llamar</span>
+                    </a>
+                    <a className="contact" href="https://www.instagram.com/guzziautos">
                         <svg xmlns="http://www.w3.org/2000/svg" className="instagram icon icon-tabler icon-tabler-brand-instagram" width="44" height="44" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <rect x="4" y="4" width="16" height="16" rx="4" />
                             <circle cx="12" cy="12" r="3" />
                             <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
                         </svg>     
-                        <a className="link instagram">Instagram</a>
-                    </article>
-                    <article className="contact">
+                        <span className="link instagram">Instagram</span>
+                    </a>
+                    <a className="contact" href="https://www.facebook.com/Guzzi-Autos-112314927616800/?ref=page_internal">
                         <svg xmlns="http://www.w3.org/2000/svg" className="facebook icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                         </svg>
-                        <a className="link facebook" style={{color:'white'}}>Facebook</a>
-                    </article>
+                        <span className="link facebook" style={{color:'white'}}>Facebook</span>
+                    </a>
                 </section>
                 <section className="map">
                     <Image src="/images/zona.png" alt="Zona de Guzzi Autos" layout="fill" object-fit="cover"/>
