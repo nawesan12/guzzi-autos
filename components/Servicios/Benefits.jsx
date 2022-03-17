8f66a9cc4058d6df1f63a090162065bc9470ada6
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-03-17 17:50:48 -0300

    asd

--- components/Servicios/Benefits.jsx
+++ components/Servicios/Benefits.jsx
@@ -7,29 +7,54 @@ export default function Benefits() {
                 <div className="svgphoto">
                     <Image src="/images/benefits/primera.webp" alt="Beneficio 1" layout="fill" object-fit="cover" />
                 </div>
-                <h4 className="title">Interior impresionante</h4>
-                <p className="description">Limpieza interna con aspiradora y nuestros accesorios especiales para auto</p>
+                <h4 className="title">Limpieza Interior</h4>
+                <p className="description">
+                    Beneficios:
+                    <section>
+                        <li>-Evitar alergias a causa de polvo y otros agentes</li>    
+                        <li>-Evitar malos olores</li>
+                        <li>-Mejorar valor en el mercado</li>
+                        <li>-Mantener </li>
+                    </section>    
+                </p>
             </article>
             <article className="benefit">
                 <div className="svgphoto">
                     <Image src="/images/benefits/segunda.webp" alt="Beneficio 2" layout="fill" object-fit="cover" />
                 </div>
-                <h4 className="title">Listo para brillar</h4>
-                <p className="description">Limpieza externa con hidrolavadora de alta presion usando nuestros productos especiales</p>
+                <h4 className="title">Lavado de carroceria</h4>
+                <p className="description">
+                    Beneficios:
+                    <section>
+                        <li>-Eliminar agentes externos que puedan dañar la pintura.</li>
+                        <li>-Mantener plasticos hidratados para alargar su vida util</li>
+                        <li>-Evitar acumulacion de oxido y corrosion</li>
+                        <li></li>
+                    </section>
+                </p>
             </article>
             <article className="benefit">
                 <div className="svgphoto">
                     <Image src="/images/benefits/tercera.webp" alt="Beneficio 3" layout="fill" object-fit="cover" />
                 </div>
-                <h4 className="title">Desinfeccion</h4>
-                <p className="description">Sanitizacion apropiada removiendo viruses y germenes tambien es parte de nuestro servicio</p>
+                <h4 className="title">Limpieza de motor</h4>
+                <p className="description">Beneficios: -Mejor respiracion del motor -Detectar perdidas de fluidos -Limpieza de fluidos de perdidas solucionadas -Hidratacion de plasticas y mangueras maltratadas por suciedad y temperatura</p>
             </article>
             <article className="benefit">
                 <div className="svgphoto">
                     <Image src="/images/benefits/cuarta.webp" alt="Beneficio 4" layout="fill" object-fit="cover" />
                 </div>
-                <h4 className="title">Brillo detallado</h4>
-                <p className="description">El pulido es una parte importante de un buen resultado, tu auto lucira suave y fresco</p>
+                <h4 className="title">Tratamientos</h4>
+                <p className="description">Beneficios: 
+                    <section>
+                        <li>-Aumentar su valor en el mercado</li>
+                        <li>-Brillo intenso</li>
+                        <li>-Rejuvenecimiento del vehiculo</li>
+                        <li>-Que permanezca mas tiempo limpio</li>
+                        <li>-Que no se deteriore la pintura con facilidad</li>
+                        <li>-Y protegerlo de agentes externos</li>
+                    </section>
+                </p>
             </article>
 
             <style jsx>{`
@@ -61,11 +86,15 @@ export default function Benefits() {
                 }
 
                 .description {
-                    text-align:center;
+                    text-align:left;
                     color:#ccc;
                     font-size:1.1rem;
                     padding:0 4rem;
                     font-weight:bold;
+                }
+
+                .description li {
+                    list-style-type: none;
                 }
 
                 @media screen and (min-width:1200px) {
